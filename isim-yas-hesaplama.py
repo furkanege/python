ae1ca1d6f00928ade903fe6af91313b146a85158
furkanege <61287972+furkanege@users.noreply.github.com>
2020-02-20 20:19:18 +0300

    Add files via upload

---
+++ isim-yas-hesaplama.py
@@ -0,0 +1,37 @@
+isim = input("İsminiz:")
+cinsiyet = str(input("Cinsiyetiniz(e/k):"))
+yas = int(input("Yaşınız:"))
+
+if (yas >= 1 and yas < 12 and cinsiyet == "e"):
+    print("Merhaba {}, yaşınız {}, daha çocuksun :)".format(isim, yas))
+if (yas >= 1 and yas < 12 and cinsiyet == "k"):
+    print("Merhaba {}, yaşınız {}, daha çocuksun :)".format(isim, yas))
+
+
+elif (yas >= 12 and yas < 28 and cinsiyet == "e"):
+    print("Merhaba {} bey, yaşınız {}, hala çok gençsiniz".format(isim, yas))
+elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+    print("Merhaba {} hanım, yaşınız {}, hala çok gençsiniz".format(isim, yas))
+
+
+elif (yas >= 28 and yas < 40 and cinsiyet == "e"):
+    print("Merhaba {} bey, yaşınız {}, yetişkinsizin".format(isim, yas))
+elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+    print("Merhaba {} hanım, yaşınız {}, yetişkinsizin".format(isim, yas))
+
+
+
+elif (yas >= 40 and yas < 60 and cinsiyet == "e"):
+    print("Merhaba {} bey, yaşınız {}, orta yaşlısınız".format(isim, yas))
+elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+    print("Merhaba {} hanım, yaşınız {}, hala çok gençsiniz".format(isim, yas))
+
+
+elif (yas >= 60 and cinsiyet == "e"):
+    print("Merhaba {} bey, yaşınız {}, ihtiyarsınız.".format(isim, yas))
+elif (yas >= 60 and cinsiyet == "k"):
+    print("Merhaba {} hanımi, yaşınız {}, hala çok gençsiniz".format(isim, yas))
+
+
+else:
+    print("Merhaba {}, yaşınız {}, hatalı giriş.".format(isim, yas))
