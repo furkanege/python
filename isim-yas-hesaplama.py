48dacae81e0674dfb4d9935af0bf5ace60d50bac
Furkan Ege <61287972+furkanege@users.noreply.github.com>
2020-03-16 21:20:39 +0300

    Update isim-yas-hesaplama.py

--- isim-yas-hesaplama.py
+++ isim-yas-hesaplama.py
@@ -2,28 +2,28 @@ isim = input("İsminiz:")
 cinsiyet = str(input("Cinsiyetiniz(e/k):"))
 yas = int(input("Yaşınız:"))
 
-if (yas >= 1 and yas < 12 and cinsiyet == "e"):
+if (yas >= 1 and yas < 12 and cinsiyet.lower() == "e"):
     print("Merhaba {}, yaşınız {}, daha çocuksun :)".format(isim, yas))
-if (yas >= 1 and yas < 12 and cinsiyet == "k"):
+if (yas >= 1 and yas < 12 and cinsiyet.lower() == "k"):
     print("Merhaba {}, yaşınız {}, daha çocuksun :)".format(isim, yas))
 
 
-elif (yas >= 12 and yas < 28 and cinsiyet == "e"):
+elif (yas >= 12 and yas < 28 and cinsiyet.lower() == "e"):
     print("Merhaba {} bey, yaşınız {}, hala çok gençsiniz".format(isim, yas))
-elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+elif (yas >= 12 and yas < 28 and cinsiyet.lower() == "k"):
     print("Merhaba {} hanım, yaşınız {}, hala çok gençsiniz".format(isim, yas))
 
 
-elif (yas >= 28 and yas < 40 and cinsiyet == "e"):
+elif (yas >= 28 and yas < 40 and cinsiyet.lower() == "e"):
     print("Merhaba {} bey, yaşınız {}, yetişkinsizin".format(isim, yas))
-elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+elif (yas >= 12 and yas < 28 and cinsiyet.lower() == "k"):
     print("Merhaba {} hanım, yaşınız {}, yetişkinsizin".format(isim, yas))
 
 
 
-elif (yas >= 40 and yas < 60 and cinsiyet == "e"):
+elif (yas >= 40 and yas < 60 and cinsiyet.lower() == "e"):
     print("Merhaba {} bey, yaşınız {}, orta yaşlısınız".format(isim, yas))
-elif (yas >= 12 and yas < 28 and cinsiyet == "k"):
+elif (yas >= 12 and yas < 28 and cinsiyet.lower() == "k"):
     print("Merhaba {} hanım, yaşınız {}, hala çok gençsiniz".format(isim, yas))
 
 
